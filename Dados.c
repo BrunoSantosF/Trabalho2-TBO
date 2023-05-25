2dc29d5f7f6e9d84d267d7f92ade6a0c8e6ba5d3
Henrique <hecaetano1@hotmail.com>
2023-05-25 20:33:17 -0300

    🐛 corringindo erros de sintaxe

--- Dados.c
+++ Dados.c
@@ -10,7 +10,7 @@ struct dados {
   int T; // nó de destino 
   int VeloInicial; //velocidade media inicial
   float ** VetorArestas; //vetor que contém (vetice origem,vertice destino,distancia entre os nós)[3]
-  int **VetorTrafego; //vetor com informacoes do trafego ao longo da viagem(instante de tempo[0], aresta origem[1], aresta destino[2], velocidade media nova[3])
+  int ** VetorTrafego; //vetor com informacoes do trafego ao longo da viagem(instante de tempo[0], aresta origem[1], aresta destino[2], velocidade media nova[3])
   int tamanhoVetorTrafego; // tamanho do vetor de trafego
 };
 
@@ -39,9 +39,9 @@ Dados * leituraArquivoEntrada(char * entrada){
   int qtd = 0;
   while (!feof(arquivo)){
     qtd++;
-    fscanf(arquivo,"%[^\n]\n",str); //capiturando tamanho de linha do arquivo 
+    fscanf(arquivo,"%[^\n]\n",str); //capturando tamanho de linha do arquivo 
   }
-  fseek(arquivo,0,SEEK_SET);
+  fseek(arquivo, 0, SEEK_SET); //retorno para início do arquivo
 
 
   fscanf(arquivo, "%d;%d\n",&dados->N,&dados->M);
@@ -55,21 +55,15 @@ Dados * leituraArquivoEntrada(char * entrada){
     fscanf(arquivo, "%f;%f;%f\n",&dados->VetorArestas[i][0],&dados->VetorArestas[i][1],&dados->VetorArestas[i][2]);
   }
 
-  //Alocando vetor de trafego 
-  dados->tamanhoVetorTrafego = qtd = qtd - (3 + dados->M); //tamanho do vetor de trafego
+  //alocando vetor de trafego 
+  dados->tamanhoVetorTrafego = qtd - (3 + dados->M); //tamanho do vetor de trafego
   dados->VetorTrafego = calloc(qtd, sizeof(int *));
+
   for (int i = 0; i < dados->tamanhoVetorTrafego; i++){
     dados->VetorTrafego[i] = calloc(4,sizeof(int));
     fscanf(arquivo,"%d;%d;%d;%d\n",&dados->VetorTrafego[i][0],&dados->VetorTrafego[i][1],&dados->VetorTrafego[i][2],&dados->VetorTrafego[i][3]);
   }
-
-
   fclose(arquivo);
 
-
   return dados;
-
 }
-
-
-
