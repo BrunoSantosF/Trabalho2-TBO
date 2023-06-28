441bee8fdebf072a64f20eea0878b72f4e019924
Matheus <mt.schreiber01@gmail.com>
2023-06-28 00:05:40 -0300

    :zap: performance improved

--- main.c
+++ main.c
@@ -3,12 +3,8 @@
 #include <string.h>
 #include "Dados.h"
 #include "FilaPrioridade.h"
-#include <time.h>
-
 
 int main(int argc, char  ** argv){
-  clock_t start = clock();
-  
   if (argc != 3){
       printf("Erro: numero de argumentos invalido\n");
       return 1;
@@ -33,26 +29,18 @@ int main(int argc, char  ** argv){
   // contando o numero de linhas antes de começar a ler os conteudos
   int nmrLinhas = nmrLinhasArquivo(arquivoEntrada);
   
-  clock_t t1 = clock();
   // populando os vetores de dados
   leituraParametros(arquivoEntrada, &N, &M, &S, &T);
   arestas = leituraArestas(arquivoEntrada, M, N);
 
   trafego = leituraTrafegos(arquivoEntrada, &tamanhoTrafego, nmrLinhas, M);
   fclose(arquivoEntrada);
-  clock_t t2 = clock();
-  printf("Tempo na leitura: %lf ms\n", ((((double)(t2-t1))/CLOCKS_PER_SEC) * 1000));
 
   // aplicando processamento do trabalho
-  clock_t t3 = clock();
   processaDados(arquivoSaida, arestas, M, N, S, T, trafego, tamanhoTrafego);
-  clock_t t4 = clock();
-  printf("Tempo no processamento: %lf ms\n", ((((double)(t4-t3))/CLOCKS_PER_SEC) * 1000));
   fclose(arquivoSaida);
   
   liberaDados(arestas, N, trafego, tamanhoTrafego);
 
-  clock_t end = clock();
-  printf("Tempo total: %lf ms\n", ((((double)(end-start))/CLOCKS_PER_SEC) * 1000));
   return 0;
 }
